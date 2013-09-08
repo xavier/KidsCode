5ba5c6e0e3f5efbd34ef51b07182e9394283b9fa
Xavier Defrang <xavier.defrang@gmail.com>
2013-09-08 15:03:56 +0200

    Bulletin sans tableau

---
+++ bulletin1.py
@@ -0,0 +1,13 @@
+from kidscode import ask
+
+note1 = int(ask("Note 1: "))
+note2 = int(ask("Note 2: "))
+note3 = int(ask("Note 3: "))
+note4 = int(ask("Note 4: "))
+note5 = int(ask("Note 5: "))
+raw_
+total = note1 + note2 + note3 + note4 + note5
+moyenne = total / 5
+
+print "Total: " + str(total) + "/100"
+print "Moyenne: " + str(moyenne) + "/20"
