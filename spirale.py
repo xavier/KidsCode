1a063de7e4656d4ecac31af7a9c257dd95092822
Xavier Defrang <xavier.defrang@gmail.com>
2013-09-08 18:05:06 +0200

    Spirale itérative vs récursive

--- spirale.py
+++ spirale.py
@@ -2,15 +2,29 @@ import turtle
 
 t = turtle.Pen()
 
-def spirale(t, segment, pourcent, angle, iterations):
+def augmente(segment, pourcent):
+  return segment + (segment*(pourcent/100.0))
+
+def spirale_recursive(t, segment, pourcent, angle, iterations):
   if iterations == 0: return
   t.forward(segment)
-  t.left(angle)
-  ajout_segment = (segment*(pourcent/100.0))
-  spirale(t, segment+ajout_segment, pourcent, angle, iterations-1)
+  t.right(angle)
+  spirale_recursive(t, augmente(segment, pourcent), pourcent, angle, iterations-1)
+
+def spirale_iterative(t, segment, pourcent, angle, iterations):
+  for i in range(iterations):
+    t.forward(segment)
+    t.right(angle)
+    segment = augmente(segment, pourcent)
 
 t.speed("fast")
 
-spirale(t, 10, 15, 98, 30)
+spirale_recursive(t, 10, 15, 50, 30)
+
+t.penup()
+t.home()
+t.pendown()
+
+spirale_iterative(t, 10, 10, 98, 50)
 
 raw_input()
